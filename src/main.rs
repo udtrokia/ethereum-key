53af965af38c7382443b079c8a0b72a53ecc561f
mercury <udtrokia@gmail.com>
2018-07-26 12:29:33 +0800

    [MIGRATE]: Migrate cli from parity-ethkey

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate ethkey_cli;
+
+use std::{env, process};
+
+use ethkey_cli::cli::{env_logger, execute, panic_hook, Error};
+
+
 fn main() {
-    println!("Hello, world!");
+    panic_hook::set_abort();
+    env_logger::init().expect("Logger initialized only once.");
+    
+    match execute(env::args()) {
+ 	Ok(ok) => println!("{}", ok),
+ 	Err(Error::Docopt(ref e)) => e.exit(),
+ 	Err(err) => {
+ 	    println!("{}", err);
+ 	    process::exit(1);
+ 	}
+    }
 }
