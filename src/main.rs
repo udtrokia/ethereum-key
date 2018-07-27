5bab3eb8852a6d2360ebf09bbbfdeaef8fe28541
mercury <udtrokia@gmail.com>
2018-07-27 12:46:27 +0800

    [CHANGE]: Abandon Client interface, move to ethkey interface directly.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
-extern crate ethkey_cli;
+extern crate ethkey;
 
-use std::{env, process};
+use self::ethkey::{Brain, BrainPrefix, Generator};
 
-use ethkey_cli::cli::{env_logger, execute, panic_hook, Error};
-
+const BRAIN_WORDS: usize = 12;
 
 fn main() {
-    panic_hook::set_abort();
-    env_logger::init().expect("Logger initialized only once.");
-    
-    match execute(env::args()) {
- 	Ok(ok) => println!("{}", ok),
- 	Err(Error::Docopt(ref e)) => e.exit(),
- 	Err(err) => {
- 	    println!("{}", err);
- 	    process::exit(1);
- 	}
+    // generate
+    let mut brain = BrainPrefix::new(vec![0], usize::max_value(), BRAIN_WORDS);
+    let keypair = brain.generate().unwrap().to_owned();
+
+    // phrase
+    let phrase = brain.phrase();
+    println!("Phrase: {:?}", &phrase);
+    let validate = validate_phrase(&phrase);
+    println!("Validate: {:?}", validate);
+
+    // secret
+    let secret = keypair.secret().to_owned();
+    println!("Secret: {:?}", secret);
+
+    // public
+    let public = keypair.public().to_owned();
+    println!("Public: {:?}", public);
+
+    // address
+    let address = keypair.address().to_owned();
+    println!("Address: {:?}", address);
+
+    // recover from phrase
+    let g = Brain::new(phrase.to_string()).generate().unwrap();
+    println!("Recover: {:?}", g.address());
+}
+
+fn validate_phrase(phrase: &str) -> bool {
+    match Brain::validate_phrase(phrase, BRAIN_WORDS) {
+	Ok(()) => true,
+	Err(_err) => false
     }
 }
